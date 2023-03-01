234bda306bc6cc8a69b060f5288d9016af1effd6
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-03-01 17:10:21 +0300

    complete assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,38 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+    # add routes
+  get '/bakeries' do
+      # get all the games from the database
+     # return a JSON response with an array of all the game data
+     # get all the games from the database
+     bakeries = Bakery.all.order(:title).limit(10)
+     # look up the game in the database using its ID
+      
+     # return a JSON response with an array of all the game data
+     bakeries.to_json
+  end
 
-  # add routes
+  get '/bakeries/:id' do
+     # get all the games from the database
+      # return a JSON response with an array of all the game data
+     # get all the games from the database
+     bakeries = Bakery.all.order(:title).limit(10)
+     # look up the game in the database using its ID
+      bakery = Bakery.find(params[:id])
+     # return a JSON response with an array of all the game data
+      bakery.to_json(
+      include: :baked_goods
+     )
+
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all.order(price: :desc)
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    BakedGood.all.order(price: :desc)[0].to_json
+  end
 
 end
